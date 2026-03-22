52e413d88e133eef57cda2319c83745be4d50a6a
nadhmi12 <nadhmi.dev@gmail.com>
2026-03-22 20:33:35 +0100

    feat: add zod validation to project

--- src/config/validation.js
+++ src/config/validation.js
@@ -6,6 +6,18 @@ export const clientSchema = z.object({
 })
 
 export const updateClientSchema = z.object({
-    name: z.string().min(3).max(10).optional(),
+    name: z.string().min(3).max(20).optional(),
     email: z.email().optional()
+})
+
+export const projectSchema = z.object({
+    description: z.string().min(20).max(400),
+    status: z.enum(["TODO", "IN_PROGRESS", "COMPLETED", "DELIVERED"]).optional(),
+    deadline: z.iso.datetime()
+})
+
+export const updateProjectSchema = z.object({
+    description: z.string().min(20).max(400).optional(),
+    status: z.enum(["TODO", "IN_PROGRESS", "COMPLETED", "DELIVERED"]).optional(),
+    deadline: z.iso.datetime().optional()
 })
